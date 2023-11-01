a0f6aa205ee235e5e49ac1cb923876e0cf0b9904
Steven McVey <smcvey1210@gmail.com>
2023-10-31 22:58:50 -0400

    update login to make working URLRequest

--- Chinese-Character-Test-iOS/Login.swift
+++ Chinese-Character-Test-iOS/Login.swift
@@ -11,6 +11,41 @@ struct Login: View {
     @State private var username = ""
     @State private var password = ""
     @State private var isLogged = false
+    @State private var role = "student"
+    
+    struct UserObject: Codable {
+        let role: String
+        let userInfo: UserInfo
+    }
+    
+    struct UserInfo: Codable {
+        let username: String
+        let password: String
+    }
+    
+    func sendLoginRequest(_ jsonData: Data){
+        
+        guard let url = URL(string: "https://www.chinesecharactertest.com/login") else {
+            print("Invalid URL")
+            return
+        }
+        var request = URLRequest(url: url)
+        
+        request.httpMethod = "POST"
+        request.setValue("application/json", forHTTPHeaderField: "Content-Type")
+        request.httpBody = jsonData
+        
+        let task = URLSession.shared.dataTask(with: request) { (data, response, error) in
+            if let error = error {
+                print(print("Error: \(error)"))
+            }else if let data = data{
+                if let responseString = String(data: data, encoding: .utf8){
+                    print("Response: \(responseString)")
+                }
+            }
+        }
+        task.resume()
+    }
     
     var body: some View {
         Text("Please log in to get started")
@@ -20,16 +55,27 @@ struct Login: View {
 //                    .resizable()
 //                    .frame(width: 100, height: 100)
 //                    .padding(.bottom, 20)
-//                
+                Section(header: Text("Role")){
+                    Picker("I am a...", selection: $role){
+                        Text("student").tag("student")
+                        Text("teacher/researcher").tag("teacher")
+                    }
+                    .pickerStyle(SegmentedPickerStyle())
+                }
                 TextField("Username", text: $username)
                     .textFieldStyle(RoundedBorderTextFieldStyle())
+                    .autocapitalization(.none)
                     .padding()
                 SecureField("Password", text: $password)
                     .textFieldStyle(RoundedBorderTextFieldStyle())
                     .padding()
                 Button(action: {
-                    if username == "admin" && password == "password" {
-                        isLogged = true
+                    let userInfo = UserInfo(username: username, password: password)
+                    let userObject = UserObject(role: role, userInfo: userInfo)
+                    
+                    if let jsonData = try?
+                        JSONEncoder().encode(userObject){
+                        sendLoginRequest(jsonData)
                     }
                 }) {
                     Text("Log In")
@@ -43,7 +89,7 @@ struct Login: View {
                 .padding()
                 
                 if isLogged {
-                  Text("Logged in successfully!")
+                  Text("\(role)")
                       .foregroundColor(.green)
                 }
                 Spacer()
